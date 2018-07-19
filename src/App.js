a91ee783191c789c5965f90a9ec1088f9d212fee
fwgterhhyr]hrlsy <Bilal.a.Haroon@gmail.com>
2018-07-19 15:30:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      value: '',
+      imageUrl: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    fetch("https://api.github.com/users/" + this.state.value)
+    .then(res => {return res.json()}).then(data => this.setState({imageUrl: data.avatar_url}))
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input type="text" name="title" onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+        <img src={this.state.imageUrl} alt=""/>
+      </div>
+    );
+  }
+}
+
+export default App;
