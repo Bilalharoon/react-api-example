c25642d61d584affa44d59b47c77044f47006896
fwgterhhyr]hrlsy <Bilal.a.Haroon@gmail.com>
2018-07-20 15:43:13 -0500

    added comments

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      value: '',
-      imageUrl: ''
+      value: '',  // the value of the text field
+      imageUrl: ''  //the url of the image that is returned from json
     }
+    // bindings
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  // triggered when input tex is changed
   handleChange(event){
+    //set state.value to the input text when it is changed
     this.setState({value: event.target.value});
   }
 
+  // triggered when form is submitted
   handleSubmit(event){
+    // make a GET request to github api and parse the JSON
     fetch("https://api.github.com/users/" + this.state.value)
     .then(res => {return res.json()}).then(data => this.setState({imageUrl: data.avatar_url}))
     event.preventDefault();
